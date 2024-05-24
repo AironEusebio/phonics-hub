0eb7639aebc65d6edfb5ff4ceb957d0d7a94e73c
Airon Eusebio <aironeusebio@gmail.com>
2024-05-24 20:53:36 +0800

    Completed 2nd Assessment

--- assessment.js
+++ assessment.js
@@ -1,27 +1,27 @@
-const assessVowels = {
-  vowels: [
-    {
-      letter: "a",
-      audio: "./audio/a.wav",
-    },
-    {
-      letter: "e",
-      audio: "./audio/e.wav",
-    },
-    {
-      letter: "i",
-      audio: "./audio/i.wav",
-    },
-    {
-      letter: "o",
-      audio: "./audio/o.wav",
-    },
-    {
-      letter: "u",
-      audio: "./audio/u.wav",
-    },
-  ],
-};
+// const assessVowels = {
+//   vowels: [
+//     {
+//       letter: "a",
+//       audio: "./audio/a.wav",
+//     },
+//     {
+//       letter: "e",
+//       audio: "./audio/e.wav",
+//     },
+//     {
+//       letter: "i",
+//       audio: "./audio/i.wav",
+//     },
+//     {
+//       letter: "o",
+//       audio: "./audio/o.wav",
+//     },
+//     {
+//       letter: "u",
+//       audio: "./audio/u.wav",
+//     },
+//   ],
+// };
 
 // const assessConsonants = {
 //   consonants: [
@@ -112,12 +112,134 @@ const assessVowels = {
 //   ],
 // };
 
-const assessConsonants = {
-  consonants: [
-    { letter: "C", audio: "./audio/c.wav" },
-    { letter: "D", audio: "./audio/d.wav" },
-    { letter: "F", audio: "./audio/f.wav" },
-    { letter: "G", audio: "./audio/g.wav" },
-    { letter: "H", audio: "./audio/h.wav" },
+const assessVowels = {
+  vowels: [
+    { letter: "A", audio: "./audio/a.wav" },
+    { letter: "E", audio: "./audio/e.wav" },
+    { letter: "I", audio: "./audio/i.wav" },
+    { letter: "O", audio: "./audio/o.wav" },
+    { letter: "U", audio: "./audio/u.wav" },
+  ],
+};
+
+const assessmentPart2 = {
+  questions: [
+    {
+      word: ["M", "A", "P"],
+      audio: [
+        { letter: "A", audio: "./audio/a.wav" },
+        { letter: "C", audio: "./audio/c.wav" },
+        { letter: "R", audio: "./audio/r.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+        { letter: "U", audio: "./audio/u.wav" },
+        { letter: "P", audio: "./audio/p.wav" },
+        { letter: "O", audio: "./audio/o.wav" },
+        { letter: "X", audio: "./audio/x.wav" },
+      ],
+    },
+    {
+      word: ["R", "A", "T"],
+      audio: [
+        { letter: "T", audio: "./audio/t.wav" },
+        { letter: "N", audio: "./audio/n.wav" },
+        { letter: "J", audio: "./audio/j.wav" },
+        { letter: "R", audio: "./audio/r.wav" },
+        { letter: "S", audio: "./audio/s.wav" },
+        { letter: "A", audio: "./audio/a.wav" },
+        { letter: "D", audio: "./audio/d.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+      ],
+    },
+    {
+      word: ["T", "E", "N"],
+      audio: [
+        { letter: "N", audio: "./audio/n.wav" },
+        { letter: "T", audio: "./audio/t.wav" },
+        { letter: "G", audio: "./audio/g.wav" },
+        { letter: "H", audio: "./audio/h.wav" },
+        { letter: "I", audio: "./audio/i.wav" },
+        { letter: "E", audio: "./audio/e.wav" },
+        { letter: "J", audio: "./audio/j.wav" },
+        { letter: "K", audio: "./audio/k.wav" },
+      ],
+    },
+    {
+      word: ["N", "E", "T"],
+      audio: [
+        { letter: "T", audio: "./audio/t.wav" },
+        { letter: "A", audio: "./audio/a.wav" },
+        { letter: "F", audio: "./audio/f.wav" },
+        { letter: "H", audio: "./audio/h.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+        { letter: "E", audio: "./audio/e.wav" },
+        { letter: "Z", audio: "./audio/z.wav" },
+        { letter: "N", audio: "./audio/n.wav" },
+      ],
+    },
+    {
+      word: ["M", "U", "D"],
+      audio: [
+        { letter: "U", audio: "./audio/u.wav" },
+        { letter: "D", audio: "./audio/d.wav" },
+        { letter: "A", audio: "./audio/a.wav" },
+        { letter: "K", audio: "./audio/k.wav" },
+        { letter: "C", audio: "./audio/c.wav" },
+        { letter: "E", audio: "./audio/e.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+        { letter: "F", audio: "./audio/f.wav" },
+      ],
+    },
+    {
+      word: ["S", "A", "T"],
+      audio: [
+        { letter: "S", audio: "./audio/s.wav" },
+        { letter: "T", audio: "./audio/t.wav" },
+        { letter: "E", audio: "./audio/e.wav" },
+        { letter: "F", audio: "./audio/f.wav" },
+        { letter: "G", audio: "./audio/g.wav" },
+        { letter: "H", audio: "./audio/h.wav" },
+        { letter: "A", audio: "./audio/a.wav" },
+        { letter: "U", audio: "./audio/u.wav" },
+      ],
+    },
+    {
+      word: ["F", "I", "G"],
+      audio: [
+        { letter: "G", audio: "./audio/g.wav" },
+        { letter: "J", audio: "./audio/j.wav" },
+        { letter: "K", audio: "./audio/k.wav" },
+        { letter: "Y", audio: "./audio/y.wav" },
+        { letter: "F", audio: "./audio/f.wav" },
+        { letter: "I", audio: "./audio/i.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+        { letter: "N", audio: "./audio/n.wav" },
+      ],
+    },
+    {
+      word: ["J", "A", "M"],
+      audio: [
+        { letter: "J", audio: "./audio/j.wav" },
+        { letter: "D", audio: "./audio/d.wav" },
+        { letter: "E", audio: "./audio/e.wav" },
+        { letter: "M", audio: "./audio/m.wav" },
+        { letter: "S", audio: "./audio/s.wav" },
+        { letter: "C", audio: "./audio/c.wav" },
+        { letter: "F", audio: "./audio/f.wav" },
+        { letter: "A", audio: "./audio/a.wav" },
+      ],
+    },
+    {
+      word: ["R", "I", "P"],
+      audio: [
+        { letter: "G", audio: "./audio/g.wav" },
+        { letter: "H", audio: "./audio/h.wav" },
+        { letter: "J", audio: "./audio/j.wav" },
+        { letter: "R", audio: "./audio/r.wav" },
+        { letter: "K", audio: "./audio/k.wav" },
+        { letter: "Y", audio: "./audio/Y.wav" },
+        { letter: "I", audio: "./audio/i.wav" },
+        { letter: "P", audio: "./audio/p.wav" },
+      ],
+    },
   ],
 };
--- index.js
+++ index.js
@@ -19,6 +19,10 @@ consonantButtons.forEach(function (button) {
   });
 });
 
+let currentSound = null;
+let currentLetter = null;
+let isCorrect = null;
+
 // Event listener for the "Play Sound" button inside the modal
 document.querySelector(".sound-btn").addEventListener("click", function () {
   var audioFile = modal.dataset.audioFile;
@@ -27,15 +31,9 @@ document.querySelector(".sound-btn").addEventListener("click", function () {
   }
 });
 
-let currentSound = null;
-let currentLetter = null;
-let isCorrect = null;
-
 function getRandomConsonant() {
-  const randomIndex = Math.floor(
-    Math.random() * assessConsonants.consonants.length
-  );
-  return assessConsonants.consonants[randomIndex];
+  const randomIndex = Math.floor(Math.random() * assessVowels.vowels.length);
+  return assessVowels.vowels[randomIndex];
 }
 
 function playAssessSound() {
@@ -51,14 +49,17 @@ function playAssessSound() {
   }
 }
 
-function showToast(message, isCorrect) {
+function showToast(message, isCorrect, words = false) {
   const toast = document.getElementById("toast");
   toast.textContent = message;
   toast.classList.toggle("correct", isCorrect);
   toast.classList.toggle("incorrect", !isCorrect);
   toast.style.display = "block";
   setTimeout(() => {
-    document.querySelector(".empty-container").innerHTML = "";
+    if (!words) {
+      document.querySelector(".empty-container").innerHTML = "";
+    }
+
     toast.style.display = "none";
   }, 2000);
 }
@@ -147,7 +148,214 @@ function showSection(sectionId) {
   document.getElementById(sectionId).style.display = "block";
 }
 
+function addLetterClickListeners() {
+  const letters = document.querySelectorAll(".letters-overlay-second .letter");
+  const lineContainer = document.querySelector(".clicked-letters-container");
+
+  // Function to handle click on currentLetters
+  function handleCurrentLetterClick(event) {
+    const clickedLetter = event.target;
+
+    // Remove the clicked letter from the lineContainer
+    lineContainer.removeChild(clickedLetter);
+
+    // Find the corresponding letter in the letters collection and remove 'in-line' class
+    letters.forEach((letter) => {
+      if (letter.textContent === clickedLetter.textContent) {
+        letter.classList.remove("in-line");
+      }
+    });
+
+    // Enable other letters if less than 3 letters in lineContainer
+    if (lineContainer.querySelectorAll(".letter").length < 3) {
+      letters.forEach((letter) => {
+        if (!letter.classList.contains("in-line")) {
+          letter.classList.remove("disabled");
+        }
+      });
+    }
+  }
+
+  letters.forEach((letter) => {
+    letter.addEventListener("click", function () {
+      const currentLetters = lineContainer.querySelectorAll(".letter");
+      if (currentLetters.length < 3 || this.classList.contains("in-line")) {
+        if (this.classList.contains("in-line")) {
+          const letterToRemove = Array.from(lineContainer.children).find(
+            (child) => child.textContent === this.textContent
+          );
+          handleCurrentLetterClick({ target: letterToRemove });
+        } else {
+          const lineLetter = document.createElement("div");
+          lineLetter.classList.add("letter");
+          lineLetter.textContent = this.textContent;
+
+          lineLetter.addEventListener("click", handleCurrentLetterClick); // Add click listener to currentLetter
+
+          lineContainer.appendChild(lineLetter);
+          this.classList.add("disabled", "in-line");
+        }
+
+        if (lineContainer.querySelectorAll(".letter").length < 3) {
+          letters.forEach((letter) => {
+            if (!letter.classList.contains("in-line")) {
+              letter.classList.remove("disabled");
+            }
+          });
+        }
+
+        if (lineContainer.querySelectorAll(".letter").length === 3) {
+          const currentWord = Array.from(
+            lineContainer.querySelectorAll(".letter")
+          )
+            .map((letter) => letter.textContent)
+            .join("");
+
+          while (lineContainer.firstChild) {
+            lineContainer.removeChild(lineContainer.firstChild);
+          }
+          letters.forEach((letter) => {
+            letter.classList.remove("disabled", "in-line");
+          });
+
+          console.log(currentWord);
+          const currentQuestionWord = Array.from(
+            assessmentPart2.questions[currentQuestionIndex].word
+          ).join("");
+          if (currentWord === currentQuestionWord) {
+            // Words match, generate new letters and sounds
+            isCorrect = new Howl({
+              src: "./audio/correct.mp3",
+            });
+
+            showToast("Correct!", true, true);
+            populateLettersOverlay(
+              Math.floor(Math.random() * assessmentPart2.questions.length)
+            );
+          } else {
+            isCorrect = new Howl({
+              src: "./audio/incorrect.mp3",
+            });
+            showToast("Incorrect!", false, true);
+          }
+          isCorrect.play();
+        }
+      }
+    });
+  });
+}
+
+addLetterClickListeners();
+
+let currentQuestionIndex = 0;
+
+// Populate the letters-overlay-second container with one question's letters
+function populateLettersOverlay(questionIndex) {
+  currentQuestionIndex = questionIndex;
+
+  const lettersOverlay = document.querySelector(".letters-overlay-second");
+  lettersOverlay.innerHTML = ""; // Clear existing content
+
+  const soundContainers = document.querySelectorAll(".sound-container-second");
+  soundContainers.forEach((container, index) => {
+    const letter = assessmentPart2.questions[questionIndex].word[index];
+    const audioObj = assessmentPart2.questions[questionIndex].audio.find(
+      (obj) => obj.letter === letter
+    );
+    if (audioObj) {
+      container.dataset.audio = audioObj.audio;
+    } else {
+      console.error(`Audio object not found for letter: ${letter}`);
+    }
+  });
+
+  if (questionIndex >= 0 && questionIndex < assessmentPart2.questions.length) {
+    const question = assessmentPart2.questions[questionIndex];
+    question.audio.forEach((letters) => {
+      const letterElement = document.createElement("div");
+      letterElement.classList.add("letter");
+      letterElement.dataset.letter = letters.letter;
+      letterElement.textContent = letters.letter;
+      lettersOverlay.appendChild(letterElement);
+    });
+    addLetterClickListeners();
+  } else {
+    console.error("Invalid question index");
+  }
+}
+
+// Example: Populate the letters overlay with the first question's letters
+populateLettersOverlay(
+  Math.floor(Math.random() * assessmentPart2.questions.length)
+);
+
+function handleSoundContainerClick() {
+  const soundContainers = document.querySelectorAll(".sound-container-second");
+
+  soundContainers.forEach((container) => {
+    container.addEventListener("click", () => {
+      const audioSrc = container.dataset.audio;
+
+      const sound = new Howl({
+        src: [audioSrc],
+      });
+
+      sound.play();
+    });
+  });
+}
+
+handleSoundContainerClick();
+
 // Initially show the home section
 document.addEventListener("DOMContentLoaded", () => {
   showSection("home");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const pagination = document.querySelector(".pagination");
+
+  function addPaginationListeners() {
+    const paginationLinks = document.querySelectorAll(
+      ".pagination .page-number"
+    );
+
+    paginationLinks.forEach((link) => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const pageNumber = this.getAttribute("data-page");
+
+        // Update active class
+        paginationLinks.forEach((link) => link.classList.remove("active"));
+        this.classList.add("active");
+
+        // Update content based on the page number
+        updateContent(pageNumber);
+      });
+    });
+  }
+
+  function updateContent(pageNumber) {
+    console.log(pageNumber);
+    switch (pageNumber) {
+      case "1":
+        document.querySelector(".image-container").style.display = "flex";
+        document.querySelector(".second-container").style.display = "none";
+        document.querySelector(".label").innerHTML = "Vowel Sounds";
+        break;
+      case "2":
+        document.querySelector(".image-container").style.display = "none";
+        document.querySelector(".second-container").style.display = "flex";
+        document.querySelector(".label").innerHTML = "Letter Sounds Blending";
+        break;
+      default:
+        content = "No content available for this page.";
+    }
+
+    // Reattach pagination event listeners
+    addPaginationListeners();
+  }
+
+  // Initial setup
+  addPaginationListeners();
 });
